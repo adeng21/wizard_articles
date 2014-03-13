2311e6e312273b4a370efb5f8ded1b58c85f7f62
adeng21 <adeng21@gmail.com>
2014-03-13 17:11:17 -0400

    refactored CSV

--- lib/wizard_article.rb
+++ lib/wizard_article.rb
@@ -1,7 +1,8 @@
+require 'csv'
+require 'pry'
 class WizardArticle
 
-  attr_accessor :title, :article
-  attr_reader :author
+  attr_reader :author, :title, :article
 
   def initialize(title, author, article)
     @title = title
@@ -9,8 +10,32 @@ class WizardArticle
     @article = article
   end
 
-  def to_csv_array
-    [title, author, article]
+  def to_csv_array(csv)
+    CSV.open(csv, 'a') do |line|
+      line << [@title, @author, @article]
+    end
+  end
+
+  class << self
+
+    def get_articles_from_csv(csv)
+      articles = []
+      CSV.foreach(csv) do |row|
+        articles << WizardArticle.new(row[0], row[1], row[2])
+      end
+      articles
+    end
+
+    def find_by_title(title, array)
+      article_contents = []
+
+      array.each do |row|
+        if row.title == title
+          article_contents.push(row.title, row.author, row.article)
+        end
+      end
+      article_contents
+    end
 
   end
 end
--- server.rb
+++ server.rb
@@ -14,39 +14,23 @@ post '/create-article' do
 
   @new_article = WizardArticle.new(params["article_title"], params["article_author"], params["article_body"])
 
-  CSV.open('articles.csv', 'a') do |csv|
-    # values = @new_article.instance_variables.map{|variable| @new_article.instance_variable_get(variable)}
-    csv << @new_article.to_csv_array
-  end
+  @new_article.to_csv_array('articles.csv')
 
   redirect '/'
 end
 
 
 get '/article-summary' do
-  @articles_csv = []
-
-#put these in own class
-  CSV.foreach('articles.csv') do |row|
-    @articles_csv << row[0]
-  end
+  @articles_csv = WizardArticle.get_articles_from_csv('articles.csv')
 
   erb :index_summary
 end
 
 get '/:article_title' do
 
-  @article_title = params["article_title"]
-  #how come params["article body"] doesn't work here
+  articles = WizardArticle.get_articles_from_csv('articles.csv')
 
-  @author = ""
-  @body = ""
-  CSV.foreach('articles.csv') do |row|
-    if row[0]== @article_title
-      @body += row[2]
-      @author += row[1]
-    end
-  end
+  @article_contents = WizardArticle.find_by_title(params["article_title"], articles)
 
   erb :index_title
 
